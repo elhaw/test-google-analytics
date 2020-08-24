d0c13596d5e82e7f3516835588c5acfa1ea5ba52
elhaw <abd.elsayed@hotmail.com>
2020-08-24 14:35:11 +0400

    add google analytics link

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US" style={{ fontSize: "14px" }}>
         <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-176147490-1"
+          ></script>
 
-          <meta property="og:site_name" content="M3ntorship" />
-          <meta property="og:type" content="website" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176147490-1"></script>
+              <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'UA-176147490-1');
+              </script>
+              `,
+            }}
+          />
         </Head>
         <body className="font-body leading-body">
           <Main />
